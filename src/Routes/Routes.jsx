223d421d9ab0e51630b073fcf4555a7c951602cf
musfiqrabby <musfiqrabby69@gmail.com>
2024-05-16 08:53:38 +0600

    first commit

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,17 @@
+import {  createBrowserRouter} from "react-router-dom";
+import Home from "../pages/Home/Home/Home";
+import Root from "../layouts/Root";
+
+ export const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Root/>,
+      children: [
+        {
+            path: '/',
+            element: <Home/>
+        }
+      ]
+    },
+  ]);
+
