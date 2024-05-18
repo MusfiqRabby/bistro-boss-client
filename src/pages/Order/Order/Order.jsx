63f5db0fef094f190db9f01e60f2d3c7fa788b2e
musfiqrabby <musfiqrabby69@gmail.com>
2024-05-18 12:13:05 +0600

    today final

--- src/pages/Order/Order/Order.jsx
+++ src/pages/Order/Order/Order.jsx
@@ -6,12 +6,18 @@ import 'react-tabs/style/react-tabs.css';
 import useMenu from "../../../hooks/UseMenu";
 import OrderTab from "../OrderTab";
 import { useParams } from "react-router-dom";
+import { Helmet } from "react-helmet-async";
 
 const Order = () => {
-   const [tabIndex, setTabIndex] = useState(0);
-   const {category} = useParams();
-   console.log(category);
+    const categories = ['salad', 'pizza', 'soup', 'dessert', 'drinks'];
+    const {category} = useParams();
+
+    const initialIndex = categories.indexOf(category);
+
+   const [tabIndex, setTabIndex] = useState(initialIndex);
    const [menu] = useMenu();
+
+
    const dessert = menu.filter(item => item.category === 'dessert')
    const soup = menu.filter(item => item.category === 'soup')
    const salad = menu.filter(item => item.category === 'salad')
@@ -20,6 +26,9 @@ const Order = () => {
    
     return (
         <div>
+            <Helmet>
+                <title>Bistro Boss | Order Food</title>
+            </Helmet>
             <Cover img={orderCoverImg}
             title={"Order Food"}></Cover>
 
